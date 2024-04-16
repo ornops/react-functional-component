bd70ae4f14d8f67c272006c6a89fbbe09efbb050
ornops <joshuaornopia@gmail.com>
2024-04-17 00:02:55 +0800

    converted other components to functions

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,15 @@
-import { Component } from "react";
 import Card from '../card/card.component'
 import './card-list.styles.css'
 
 
-class CardList extends Component{
-    render(){
-        const {monsters} = this.props
-        return (
-        <div className="card-list">
-            {monsters.map((monster) => {
-                return <Card monster={monster}/>
-            })}
-        </div>
-        );
-    }
-}
+const CardList = ({monsters}) =>( //you can immediately destructure props in the parameter
+<div className="card-list">
+        {monsters.map((monster) => {
+            return <Card monster={monster}/>
+        })}
+    </div>
+) 
+    
+
 
 export default CardList;
