a4578560f136706c0e8c0ce6fb678a14bb59a8a7
ornops <joshuaornopia@gmail.com>
2024-04-17 15:23:47 +0800

    Converted remaining components to functions

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ const App = () =>{
   },[]);
 
   useEffect(()=>{
-    const newfilteredMonsters = monsters.filter((monster)=>{
+    const newFilteredMonsters = monsters.filter((monster)=>{
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
-    setFilterMonsters(newfilteredMonsters)
+    setFilterMonsters(newFilteredMonsters)
   },[monsters,searchField]);
 
 
@@ -36,44 +36,9 @@ const App = () =>{
       <SearchBox className='search-box' onChangeHandler={onSearchChange} placeholder='Search Monsters'/>
       <CardList monsters={filteredMonsters}/>
     </div>
-  )
-}
+  );
+};
 
-  // componentDidMount(){
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then(response => response.json())
-  //   .then((users)=> this.setState(()=>{
-  //     return{monsters: users}
-  //   }
-  //   ));
-  // }
-
-  // onSearchChange = (event)=>{ 
-  //   // console.log(event.target.value);
-  //   const searchField = event.target.value.toLocaleLowerCase();
-    
-  //     this.setState(()=>{
-  //       return {searchField};
-  //     });
-  //   };
-
-  // render(){
-  //   console.log('render from App JS');
-  //   const {monsters,searchField} = this.state;
-  //   const {onSearchChange} = this;
-  //   const filteredMonsters =this.state.monsters.filter((monster)=>{
-  //     return monster.name.toLocaleLowerCase().includes(searchField);
-  //   });
-
-    // return (
-    // <div className="App">
-    //   <h1 className='app-title'>Monster's Academy</h1>
-    //   <SearchBox className='search-box' onChangeHandler={onSearchChange} placeholder='Search Monsters'/>
-    //   <CardList monsters={filteredMonsters}/>
-    // </div>
-    // );
-//   }
-// }
-
+ 
 export default App;
 
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,14 @@
-import Card from '../card/card.component'
-import './card-list.styles.css'
+import Card from '../card/card.component';
+import './card-list.styles.css';
 
 
 const CardList = ({monsters}) =>( //you can immediately destructure props in the parameter
 <div className="card-list">
         {monsters.map((monster) => {
-            return <Card monster={monster}/>
+            return <Card key={monster.id} monster={monster}/>
         })}
     </div>
-) 
+);
     
 
 
